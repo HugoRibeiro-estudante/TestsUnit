1ffa5f1246a26cb9d178df43b3f0018f2508104f
hugo.alves@estudante.iftm.edu.br <hugo.alves@estudante.iftm.edu.br>
2023-03-30 17:22:37 -0300

    Primeiro comentario, estruturas basicas criadas

---
+++ src/test/java/FuncionarioTerceirizadoTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+public class FuncionarioTerceirizadoTest {
+
+    @Test
+    public void testarConstrutorEntradaDespesasInvalida(){
+
+    }
+
+    @Test
+    public void testarConstrutorEntradasValidas(){
+
+    }
+
+    @Test
+    public void testarModificarDespesasEntradaInvalida(){
+
+    }
+
+    @Test
+    public void testarModificarDespesasEntradavalida(){
+
+    }
+
+
+}
---
+++ src/test/java/FuncionarioTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Test;
+
+public class FuncionarioTest {
+
+    @Test
+    public void testarConstrutorPagamentoInvalido(){
+
+    }
+
+    @Test
+    public void testarConstrutorEntradaValorHoraInvalida(){
+
+    }
+
+    @Test
+    public void testarConstrutorEntradaHorasInvalida(){
+
+    }
+
+    @Test
+    public void TestarConstrutorEntradasValida(){
+
+    }
+
+    @Test
+    public void testarModificarHorasPagamentoInvalido(){
+
+    }
+
+    @Test
+    public void testarModificarHorasEntradavalida(){
+
+    }
+
+
+    @Test
+    public void testarModificarValorPagamentoInvalido(){
+
+    }
+
+
+    @Test
+    public void testarModificarValorEntradaInvalida(){
+
+    }
+
+    @Test
+    public void testarModificarValorEntradaValida(){
+
+    }
+
+}
