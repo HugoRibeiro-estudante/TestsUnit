a31280b25a6ae8c2c1714e27bc49b7c43844e9e8
hugo.alves@estudante.iftm.edu.br <hugo.alves@estudante.iftm.edu.br>
2023-04-10 22:24:09 -0300

    criadas as primeiras istancias

---
+++ src/main/java/FuncionarioTerceirizado.java
@@ -0,0 +1,11 @@
+public class FuncionarioTerceirizado extends Funcionario{
+
+    private double despesasAdicionais;
+
+    public FuncionarioTerceirizado(String nome, int horasTrabalhadas, double valorHora, double despesasAdicionais) {
+        super(nome, horasTrabalhadas, valorHora);
+        this.despesasAdicionais = despesasAdicionais;
+    }
+
+
+}
--- src/test/java/FuncionarioTerceirizadoTest.java
+++ src/test/java/FuncionarioTerceirizadoTest.java
@@ -1,19 +1,24 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class FuncionarioTerceirizadoTest {
-
     @Test
     public void testarConstrutorEntradaDespesasInvalida(){
+
+        FuncionarioTerceirizado terceirizado = new FuncionarioTerceirizado("Hugo", 120, 10, -1);
 
     }
 
     @Test
     public void testarConstrutorEntradasValidas(){
 
+        FuncionarioTerceirizado terceirizado = new FuncionarioTerceirizado("Hugo", 120, 10, 20);
+
     }
 
     @Test
     public void testarModificarDespesasEntradaInvalida(){
+
 
     }
 
