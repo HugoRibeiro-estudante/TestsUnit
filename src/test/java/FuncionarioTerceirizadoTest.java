481c702349f1397932af543ba125709fe13dbfb9
HugoRibeiro-estudante <hugo.alves@estudante.iftm.edu.br>
2023-04-12 18:45:31 -0300

    criados alguns testes

--- src/test/java/FuncionarioTerceirizadoTest.java
+++ src/test/java/FuncionarioTerceirizadoTest.java
@@ -1,30 +1,39 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FuncionarioTerceirizadoTest {
     @Test
     public void testarConstrutorEntradaDespesasInvalida(){
 
-        FuncionarioTerceirizado terceirizado = new FuncionarioTerceirizado("Hugo", 120, 10, -1);
-
+        assertThrows(IllegalArgumentException.class, ()-> {FuncionarioTerceirizado terceirizado = new FuncionarioTerceirizado("Hugo", 120, 10, true);});
     }
 
     @Test
     public void testarConstrutorEntradasValidas(){
 
         FuncionarioTerceirizado terceirizado = new FuncionarioTerceirizado("Hugo", 120, 10, 20);
-
+        assertEquals("Hugo", terceirizado.getNome());
+        assertEquals(120, terceirizado.getHorasTrabalhadas());
+        assertEquals(10, terceirizado.getValorHora());
+        assertEquals(20, terceirizado.getDespesasAdicionais());
     }
 
     @Test
     public void testarModificarDespesasEntradaInvalida(){
 
+        FuncionarioTerceirizado terceirizado = new FuncionarioTerceirizado("Hugo", 120, 10, 20);
+        assertThrows(IllegalArgumentException.class, ()-> {terceirizado.setDespesasAdicionais("30");});
 
     }
 
     @Test
     public void testarModificarDespesasEntradavalida(){
-
+        FuncionarioTerceirizado terceirizado = new FuncionarioTerceirizado("Hugo", 120, 10, 20);
+        assertEquals(10,terceirizado.setDespesasAdicionais(10));
     }
 
 
--- src/test/java/FuncionarioTest.java
+++ src/test/java/FuncionarioTest.java
@@ -1,4 +1,8 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FuncionarioTest {
 
@@ -10,26 +14,44 @@ public class FuncionarioTest {
     @Test
     public void testarConstrutorEntradaValorHoraInvalida(){
 
+        Funcionario func = new Funcionario("Hugo", 40, 10);
+
+        assertThrows(IllegalArgumentException.class, ()-> {func.getvalorHora()*func.gethorasTrabalhadas();});
+
     }
 
     @Test
     public void testarConstrutorEntradaHorasInvalida(){
+
+        Funcionario func = new Funcionario("Hugo", 41, 10);
+
+
+        assertThrows(IllegalArgumentException.class, ()-> {});
 
     }
 
     @Test
     public void TestarConstrutorEntradasValida(){
 
+        Funcionario func = new Funcionario("Hugo", 41, 10);
+
+        assertEquals("Hugo", func.getNome());
+        assertEquals(41, func.getHorasTrabalhadas());
+        assertEquals(10, func.getValorHora());
+
     }
 
     @Test
-    public void testarModificarHorasPagamentoInvalido(){
+    public void testarModificarHorasEntradaInvalido(){
 
+        Funcionario func = new Funcionario("Hugo", 41, 10);
+        assertThrows(IllegalArgumentException.class, ()-> {func.setHorasTrabalhadas(42);});
     }
 
     @Test
     public void testarModificarHorasEntradavalida(){
-
+        Funcionario func = new Funcionario("Hugo", 41, 10);
+        assertThrows(30, ()-> {func.setHorasTrabalhadas(30);});
     }
 
 
@@ -42,6 +64,8 @@ public class FuncionarioTest {
     @Test
     public void testarModificarValorEntradaInvalida(){
 
+        Funcionario func = new Funcionario("Hugo", 41, 10);
+        assertThrows(IllegalArgumentException.class, ()-> {func.setValorHora(1);});
     }
 
     @Test
